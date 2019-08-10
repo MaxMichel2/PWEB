8d763c479016399fe128614f9b579c85ac2570c1
Maxime <maxime_michel_uk@hotmail.com>
2019-08-10 10:19:13 +0200

    Start of URL modifications

--- exchange/urls.py
+++ exchange/urls.py
@@ -13,12 +13,12 @@ urlpatterns = [
     path('continent/<str:cont>', views.pays, name='pays'),
     path('pays/<int:pays>', views.villes, name='villes'),
     path('ville/<int:ville>', views.univs, name='univs'),
-    path('modifier/<int:univ>', views.rajoutInfo, name='rajoutInfo'),
-    path('modifDepStud/<int:univ>/<int:stud>', views.rajoutInfo2, name='rajoutInfo2'),
-    path('modifExch/<int:univ>/<int:stud>', views.rajoutInfo3, name='rajoutInfo3'),
-    path('modifFinancial/<int:univ>/<int:exch>', views.rajoutInfo4, name='rajoutInfo4'),
-    path('connexion', views.connexion, name='connexion'),
-    path('deconnexion', views.deconnexion, name='deconnexion'),
+    path('modifier/<int:univ>', views.rajoutInfo, name='rajoutInfo'), #universite/<int:univ>/informations-personnelles
+    path('modifDepStud/<int:univ>/<int:stud>', views.rajoutInfo2, name='rajoutInfo2'), #universite/<int:univ>/departement
+    path('modifExch/<int:univ>/<int:stud>', views.rajoutInfo3, name='rajoutInfo3'), #universite/<int:univ>/echange
+    path('modifFinancial/<int:univ>/<int:exch>', views.rajoutInfo4, name='rajoutInfo4'), #universite/<int:univ>/bourses
+    path('login', views.connexion, name='connexion'),
+    path('logout', views.deconnexion, name='deconnexion'),
     path('rapport', views.rapport),
     path('base', views.base)
 ]
