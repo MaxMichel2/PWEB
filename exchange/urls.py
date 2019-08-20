71d82ad6b5b7c54635e9cd8571ae95b4b3608adb
Maxime <maxime_michel_uk@hotmail.com>
2019-08-20 17:28:20 +0200

    url change ideas

--- exchange/urls.py
+++ exchange/urls.py
@@ -14,9 +14,9 @@ urlpatterns = [
     path('pays/<int:pays>', views.villes, name='villes'),
     path('ville/<int:ville>', views.univs, name='univs'),
     path('modifier/<int:univ>', views.rajoutInfo, name='rajoutInfo'), #universite/<int:univ>/informations-personnelles
-    path('modifDepStud/<int:univ>/<int:stud>', views.rajoutInfo2, name='rajoutInfo2'), #universite/<int:univ>/departement
-    path('modifExch/<int:univ>/<int:stud>', views.rajoutInfo3, name='rajoutInfo3'), #universite/<int:univ>/echange
-    path('modifFinancial/<int:univ>/<int:exch>', views.rajoutInfo4, name='rajoutInfo4'), #universite/<int:univ>/bourses
+    path('modifDepStud/<int:univ>/<int:stud>', views.rajoutInfo2, name='rajoutInfo2'), #universite/<int:univ>/<int:stud>/departement
+    path('modifExch/<int:univ>/<int:stud>', views.rajoutInfo3, name='rajoutInfo3'), #universite/<int:univ>/<int:stud>/echange
+    path('modifFinancial/<int:univ>/<int:exch>', views.rajoutInfo4, name='rajoutInfo4'), #universite/<int:univ>/<int:exch>/bourses
     path('login', views.connexion, name='connexion'),
     path('logout', views.deconnexion, name='deconnexion'),
     path('rapport', views.rapport),
