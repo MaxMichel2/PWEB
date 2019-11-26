df244f5e5124bb0ca64df3bcc1492534e98db4f4
Maxime <maxime_michel_uk@hotmail.com>
2019-11-26 22:08:24 +0100

    minor setting.py update

--- PWEB/settings.py
+++ PWEB/settings.py
@@ -84,7 +84,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'pweb_international',
 		'USER': 'root',
-		'PASSWORD': 'pweb',
+        'PASSWORD': 'root',
+		#'PASSWORD': 'pweb',
 		'HOST': '127.0.0.1',
 		'PORT': '3306',
     }
@@ -128,10 +129,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = ''
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-)
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+
 
 AUTHENTICATION_BACKENDS = (
     'django_cas_ng.backends.CASBackend',
