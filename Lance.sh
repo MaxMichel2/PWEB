66270c9b43b40187c392bb179b8e4cc65628a32e
sfrenot <stephane.frenot@insa-lyon.fr>
2019-12-04 13:50:02 +0100

    Configuration cors

--- Lance.sh
+++ Lance.sh
@@ -18,6 +18,8 @@ python manage.py collectstatic
 
 -- Lancer le serveur node express pour servire les pages statiques
 coffee ./serveur.coffee --> listen sur 8001
+-- Autoriser cors pour les accès fonts
+npm install cors
 
 export LD_LIBRARY_PATH=/home/sfrenot/pweb/mysql-8.0.18-linux-x86_64-minimal/lib
 python manage.py runserver tc-net3.insa-lyon.fr:8000
--- server.coffee
+++ server.coffee
@@ -1,5 +1,11 @@
 express = require('express')
+cors = require('cors')
 app = express()
+corsOptions =
+  origin: 'http://tc-net3.insa-lyon.fr:8000'
+  credential: true
+
+app.use(cors(corsOptions))
 app.use(express.static('static'))
 app.listen 8001
 
